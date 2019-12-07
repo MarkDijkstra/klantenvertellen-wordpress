8c7e11cfb6ac8cba839e09186de386741aea9c40
MarkDijkstra <markdijkstra.eu@gmail.com>
2019-12-07 20:30:07 +0100

    added output page of reviews

--- pages/reviews.php
+++ pages/reviews.php
@@ -1,46 +1,112 @@
-<?php
-   
-    if ( isset( $_POST['submit'] ) && !empty($_POST['xmlsource'])) {        
-        
-        WP_Klantenvertellen_Process::processSource($_POST['xmlsource']);
-
-        ?>
-
-        <div id="message" class="updated notice is-dismissible">
-            <p><?php _e('Reviews opgeslagen', 'klantenvertellen'); ?></p><button type="button" class="notice-dismiss">
-            <span class="screen-reader-text">Dismiss this notice.</span></button>
-        </div>
-
-<?php } ?>
+<?php $reviews = WP_Klantenvertellen_Results::getReviews(1);?>
 
 <div class="wrap">
     <h1 class="wp-heading-inline">
         <?php _e('Reviews', 'klantenvertellen'); ?>
     </h1>
-    <br/>
-    <br/>
-    <form action="<?php esc_url( $_SERVER['REQUEST_URI'] ) ?>" method="post">
+</div>
 
-        <div id="col-container">
-            <?php 
-            
-                if (!ini_get('allow_url_fopen')) {
 
-                    echo 'allow_url_fopen is not set to On, you need to change the php.ini file for this!';
-                }
+<div class="wrap">
 
-            
-            ?>
-            
-            <div id="col-left">
-                <div class="form-field form-required">
-                    <label for="field-1">Klantenvertellen Source URL:</label>
-                    <input type="text" id="field-1" name="xmlsource" value="<?= $_POST['xmlsource']?>"/>
-                </div>
-                <br/>
-                <button type="submit" class="button button-primary" name="submit">Save</button>
-            </div>
-        </div>
+     <h3>Reviews</h3>
 
-    </form>
+     <table class="wp-list-table widefat fixed striped pages kv--table">
+         <thead>
+            <tr>             
+                <th style="width:60px">Id</th>  
+                <th>Reviewer:</th>
+                <th>Ervaring</th>
+                <th>Beoordelingen</th>
+                <th style="width:100px">Aanbevelen</th> 
+                <th style="width:60px">Cijfer</th>
+            </tr>
+         </thead>
+         <tbody>
+             <?php foreach($reviews as $review){
+             
+             
+                 $contents = WP_Klantenvertellen_Results::getContentByReviewId($review['reviewid']);
+    
+                 ?>
+             
+                <tr class="review__block" data-id="<?= $review['id'] ?>" data-reiewid="<?= $review['reviewid'] ?>" 
+                    data-updated="<?= date('d-m-Y H:i:s', strtotime($review['updatedsince'])) ?>">
+                    <td><?= $review['id'] ?></td>
+                    <td>
+                        <div class="td__row">
+                            <strong>Author: </strong><?= $review['reviewauthor'] ?>
+                        </div>
+                        <div class="td__row">
+                            <strong>Woonplaats: </strong><?= $review['city'] ?>
+                        </div>
+                        <div class="td__row">
+                            <strong>Datum: </strong><?= date('d-m-Y H:i:s', strtotime($review['datesince'])) ?>
+                        </div>                        
+                    </td> 
+                    <td>
+                        <?php foreach($contents as $content){
+                                                     
+                          if($content['questiongroup'] === 'DEFAULT_OPINION'){ ?>
+<!--                            <div class="td__row">-->
+<!--                                <strong>-->
+                                    <?php //echo $content['questiontranslation']; ?>
+<!--                                </strong>-->
+<!--                            </div>-->
+                            <div class="td__row">
+                                <?= $content['rating']; ?>
+                            </div>
+                         <?php }     
+                        } 
+                        
+                        ?>    
+    
+                    </td>
+                    <td>
+                        
+                         <div class="td__row"> 
+                        
+                            <?php foreach($contents as $content){
+
+                            $acceptTypes =['CATEGORY', 'CUSTOM'];
+
+                               if(in_array($content['questiongroup'] , $acceptTypes)){ ?>
+
+                                <div class="td__row--2">
+                                    <strong><?=$content['questiontranslation']?></strong>
+                                    <span><?= $content['rating']; ?></span>
+                                </div>
+
+                               <?php }
+                            
+                               if($content['questiongroup'] === 'DEFAULT_RECOMMEND'){
+                            
+                                    $reccommend = $content['rating'];
+                                }
+
+                             } ?> 
+                        </div>
+
+                    </td>
+                    <td>
+                        <?php 
+    
+                        if($reccommend == true){
+                            echo '<i class="fa fa-check icon--green"></i>';
+                        }else{
+                            echo '<i class="fa fa-times icon--red"></i>';
+                        }
+                        ?>
+                        
+                    </td> 
+                    <td>
+                        <div class="rating__circle">
+                            <?= $review['rating'] ?>
+                        </div>
+                    </td> 
+                </tr>
+             <?php } ?>
+         </tbody>
+    </table>   
+
 </div>
